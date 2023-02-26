b49d2f63fb77fa6025e68fc0592b55f425a75856
Caitlyn <Michiyo.Ravencroft@gmail.com>
2023-02-26 14:37:37 -0800

    maybe build number based version?

--- src/main/java/com/pclogix/opencargo/OpenCargo.java
+++ src/main/java/com/pclogix/opencargo/OpenCargo.java
@@ -15,14 +15,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod.EventBusSubscriber
-@Mod(modid = OpenCargo.MODID, name = OpenCargo.NAME, version = OpenCargo.VERSION,
+@Mod(modid = OpenCargo.MODID, name = OpenCargo.NAME, version = OpenCargo.VERSION - OpenCargo.BUILDNUMBER,
         dependencies = "required-after:opencomputers;")
 public
 class OpenCargo
 {
     public static final String MODID = "opencargo";
     public static final String NAME = "Open Cargo";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "@VERSION@";
+	public static final String BUILDNUMBER = "@BUILD@";
 
     @SidedProxy(clientSide = "com.pclogix.opencargo.proxy.ClientProxy", serverSide = "com.pclogix.opencargo.proxy.ServerProxy")
     public static CommonProxy proxy;
